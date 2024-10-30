217081482071d457cd2ab752621ce392186b8305
Renan Vizza Campos <renanvcampos@gmail.com>
2024-10-29 23:05:14 -0300

    feat: adding prefixed_uris + fixing spaced urls

--- src/config.rs
+++ src/config.rs
@@ -13,7 +13,7 @@ impl Config {
         blabla.first().unwrap().clone()
     }
 
-    pub fn get_profile_by_domain(&self, url: Url) -> ProfileConfig {
+    pub fn get_profile_by_domain(&self, url: &Url) -> ProfileConfig {
         if url.domain().is_none() {
             return self.get_default_profile();
         }
@@ -25,23 +25,13 @@ impl Config {
         }
         let mut ret: Option<ProfileConfig> = None;
         for item in self.profile.clone().into_iter() {
-            if item.domains.is_none() {
-                continue;
+            if item.domains.is_some() && item.do_i_like_this_domain(&search_domain) {
+                ret = Some(item.clone());
+                break;
             }
 
-            for domain_item in item.clone().domains.unwrap().into_iter() {
-                let mut check_domain = domain_item.to_string();
-                if domain_item.starts_with("*.") {
-                    check_domain = domain_item[2..].to_string()
-                }
-
-                if check_domain == search_domain || search_domain.ends_with(check_domain.as_str()) {
-                    ret = Some(item);
-                    break;
-                }
-            }
-
-            if ret.is_some() {
+            if item.prefixed_uris.is_some() && item.do_i_like_this_url(&url.to_string()) {
+                ret = Some(item.clone());
                 break;
             }
         }
@@ -55,5 +45,35 @@ pub struct ProfileConfig {
     pub name: String,
     pub exec: String,
     pub domains: Option<Vec<String>>,
+    pub prefixed_uris: Option<Vec<String>>,
     pub args: Option<Vec<String>>,
+}
+
+impl ProfileConfig {
+    pub(crate) fn do_i_like_this_domain(&self, domain: &str) -> bool {
+        for domain_item in self.clone().domains.unwrap().into_iter() {
+            let mut check_domain = domain_item.to_string();
+            if domain_item.starts_with("*.") {
+                check_domain = domain_item[2..].to_string()
+            }
+
+            if check_domain == domain || domain.ends_with(check_domain.as_str()) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    pub(crate) fn do_i_like_this_url(&self, url: &str) -> bool {
+        for uri_prefix in self.clone().prefixed_uris.unwrap().into_iter() {
+            let check_uri = uri_prefix.to_string();
+
+            if url.starts_with(&check_uri) {
+                return true;
+            }
+        }
+
+        false
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,67 @@ use toml::de::Error;
 
 use crate::config::{Config};
 use std::fs;
-use std::process::exit;
+use std::process::{exit, Stdio};
 use url::Url;
 use std::process::Command;
-use log::{error, warn};
-
+use flexi_logger::{Duplicate, FileSpec, WriteMode};
+use log::{error, warn, debug, LevelFilter, info};
 
 fn main() {
-    pretty_env_logger::init();
-
     let home = std::env::var("HOME").unwrap();
+    let default_folder = format!("{home}/.config/simple-browser-chooser");
+    configure_logging(&default_folder);
+    let config = build_config(&default_folder);
+    let arg_url = get_treated_parameter();
+    let mut parsed_url = Url::parse(&arg_url).unwrap();
+
+    if parsed_url.domain().is_some() && parsed_url.domain().unwrap() == "www.google.com" && parsed_url.path().ends_with("url") {
+        let qps: std::collections::HashMap<_, _> = parsed_url.query_pairs().into_owned().collect();
+        if qps.contains_key("url") {
+            let new_uri = qps.get("url").expect("url parameter is expected");
+            parsed_url = Url::parse(new_uri.as_str()).unwrap();
+        }
+    }
+
+    let the_profile = config.get_profile_by_domain(&parsed_url);
+    let cmd = the_profile.exec;
+
+    let mut uri = parsed_url.to_string();
+    if uri.contains("%20") {
+        uri = format!("'{}'", parsed_url);
+    }
+    debug!("Uri for configured browser is: {}", uri);
+
+    let mut cmd_builder = Command::new(cmd);
+    if the_profile.args.is_some() {
+        cmd_builder.args(the_profile.args.unwrap());
+    }
+    cmd_builder.arg(uri);
+    let code = cmd_builder
+        .status().unwrap();
+    exit(code.code().unwrap());
+}
+
+
+fn validate(toml_str: &str) -> Result<Config, Error> {
+    toml::from_str(toml_str)
+}
+
+fn configure_logging(default_folder: &str) {
+    let log_file = format!("{default_folder}/sbclog.log");
+    let _logger = flexi_logger::Logger::try_with_env().unwrap()
+        .log_to_file(FileSpec::try_from(log_file).unwrap()).print_message()
+        .duplicate_to_stdout(Duplicate::Info)
+        .write_mode(WriteMode::Direct)
+        .start().unwrap();
+}
+
+fn build_config(default_folder: &str) -> Config {
     let custom_config_path = std::env::var("SIMPLE_BROWSER_CONFIG");
     let file_path = custom_config_path
-        .unwrap_or_else( |_e1| {
+        .unwrap_or_else(|_e1| {
             warn!("Using $HOME/.config/simple-browser-chooser/configuration.toml as config location");
-            format!("{home}/.config/simple-browser-chooser/configuration.toml").to_string()
+            format!("{default_folder}/configuration.toml").to_string()
         });
 
     let toml_string = match fs::read_to_string(file_path.clone()) {
@@ -28,16 +74,21 @@ fn main() {
         }
     };
 
-    let config = match validate(&toml_string) {
+    match validate(&toml_string) {
         Ok(resp) => resp,
         Err(e) => {
             error!("Failed to parse config: {}", e);
             exit(2);
         }
-    };
+    }
+}
 
+fn get_treated_parameter() -> String {
     let mut arg_url = std::env::args().nth(1)
         .unwrap_or_else(|| "https://duckduckgo.com/".to_string());
+    debug!("Received arg uri: {}", arg_url);
+
+    arg_url = arg_url.replace("%20", " ");
 
     if !arg_url.starts_with("http://")
         && !arg_url.starts_with("https://")
@@ -45,29 +96,7 @@ fn main() {
         arg_url = format!("https://{}", arg_url);
     }
 
-    let mut parsed_url = Url::parse(arg_url.as_str()).unwrap();
+    debug!("Treated arg uri: {}", arg_url);
 
-    if parsed_url.domain().is_some() && parsed_url.domain().unwrap() == "www.google.com" && parsed_url.path().ends_with("url") {
-        let qps: std::collections::HashMap<_, _>= parsed_url.query_pairs().into_owned().collect();
-        if qps.contains_key("url") {
-            let new_uri = qps.get("url").expect("url parameter is expected");
-            parsed_url = Url::parse(new_uri.as_str()).unwrap();
-        }
-    }
-
-    let uri = parsed_url.to_string();
-    let the_profile = config.get_profile_by_domain(parsed_url);
-    let cmd = the_profile.exec;
-
-
-    let code = Command::new(cmd)
-        .args(the_profile.args.unwrap_or_else(Vec::new))
-        .arg(uri)
-        .status().unwrap();
-    exit(code.code().unwrap());
-}
-
-
-fn validate(toml_str: &str) -> Result<Config, Error> {
-    toml::from_str(toml_str)
+    arg_url
 }
